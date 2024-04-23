4c70f40272f2ffb7e9e3f1782f77d56b02f0aa7d
Alberto Lopez Herraiz <alopezherraiz@educa.madrid.org>
2024-04-24 00:42:00 +0200

    creados getmappings de forma general

--- tienda/src/main/java/org/grupo1/tienda/controller/ControllerRegistroCliente.java
+++ tienda/src/main/java/org/grupo1/tienda/controller/ControllerRegistroCliente.java
@@ -1,15 +1,38 @@
 package org.grupo1.tienda.controller;
 
+import jakarta.servlet.http.HttpSession;
+import org.grupo1.tienda.model.entity.Cliente;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ControllerRegistroCliente {
 @GetMapping("/datos-personales")
-    public ModelAndView datosPersonales(){
-
-        ModelAndView modelAndView = new ModelAndView();
+    public ModelAndView datosPersonales(ModelAndView modelAndView, HttpSession sesionRegistro,
+                                        @ModelAttribute("cliente") Cliente cliente) {
+        Cliente clienteRegistro = (Cliente) sesionRegistro.getAttribute("cliente");
+        modelAndView.setViewName("registro-datos-personales");
+        return modelAndView;
+    }
+    @PostMapping("/datos-personales")
+    public ModelAndView datosPersonalesPost(ModelAndView modelAndView, HttpSession sesionRegistro){
+        modelAndView.setViewName("redirect:/datos-contacto");
+        return modelAndView;
+    }
+    @GetMapping("/datos-contacto")
+    public ModelAndView datosContacto(ModelAndView modelAndView, HttpSession sesionRegistro,
+                                        @ModelAttribute("cliente") Cliente cliente) {
+        Cliente clienteRegistro = (Cliente) sesionRegistro.getAttribute("cliente");
+        modelAndView.setViewName("registro-datos-personales");
+        return modelAndView;
+    }
+    @GetMapping("/datos-cliente")
+    public ModelAndView datosCliente(ModelAndView modelAndView, HttpSession sesionRegistro,
+                                        @ModelAttribute("cliente") Cliente cliente) {
+        Cliente clienteRegistro = (Cliente) sesionRegistro.getAttribute("cliente");
         modelAndView.setViewName("registro-datos-personales");
         return modelAndView;
     }
