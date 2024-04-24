0ba8984f27ae40e4ac4cb8726e95d3990dc835c4
Alopezherraiz <alopezherraiz@educa.madrid.org>
2024-04-24 10:25:43 +0200

    pais de nacimiento select desde BBDD funcionando

--- tienda/src/main/java/org/grupo1/tienda/controller/ControllerRegistroCliente.java
+++ tienda/src/main/java/org/grupo1/tienda/controller/ControllerRegistroCliente.java
@@ -1,19 +1,29 @@
 package org.grupo1.tienda.controller;
 
 import jakarta.servlet.http.HttpSession;
+import org.grupo1.tienda.model.catalog.Pais;
 import org.grupo1.tienda.model.entity.Cliente;
+import org.grupo1.tienda.repository.PaisRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @Controller
 public class ControllerRegistroCliente {
+    @Autowired
+    PaisRepository paisRepository;
 @GetMapping("/datos-personales")
     public ModelAndView datosPersonales(ModelAndView modelAndView, HttpSession sesionRegistro,
                                         @ModelAttribute("cliente") Cliente cliente) {
+        List<Pais> paisLista = paisRepository.findAll();
         Cliente clienteRegistro = (Cliente) sesionRegistro.getAttribute("cliente");
+        modelAndView.addObject("paises", paisLista);
+
         modelAndView.setViewName("registro-datos-personales");
         return modelAndView;
     }
