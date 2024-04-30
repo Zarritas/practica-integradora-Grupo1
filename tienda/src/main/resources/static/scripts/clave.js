b68e9f85ada1ab75c13f8bb36b23e5f624ed2877
Azufer <ana.zurita.fdez@gmail.com>
2024-04-30 11:03:07 +0200

    Commit 2 para guardar la información por si las moscas

---
+++ tienda/src/main/resources/static/scripts/clave.js
@@ -0,0 +1,7 @@
+const pregunta = document.getElementById("pregunta");
+const respuesta = document.getElementById("respuesta");
+const boton = document.getElementById("boton");
+
+function mostrarPregunta() {
+
+}
