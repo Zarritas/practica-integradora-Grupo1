db7853475b1d8175bfb2209bf7ecfd3516ccac9b
Azufer <ana.zurita.fdez@gmail.com>
2024-05-01 18:07:12 +0200

    Commit 3 para guardar la información por si las moscas

--- tienda/src/main/resources/static/scripts/clave.js
+++ tienda/src/main/resources/static/scripts/clave.js
@@ -3,5 +3,8 @@ const respuesta = document.getElementById("respuesta");
 const boton = document.getElementById("boton");
 
 function mostrarPregunta() {
-
+    let preg = document.createElement("input");
+    preg.setAttribute("type", "text");
+    preg.setAttribute("value", "pregunta");
+    pregunta.appendChild("preg");
 }
