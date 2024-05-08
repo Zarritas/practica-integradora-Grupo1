a13767a945b44cc3a567fe0161769d953a1e104f
Azufer <ana.zurita.fdez@gmail.com>
2024-05-08 13:26:57 +0200

    Eliminación de las entidades que se van a guardar en MongoDB

--- tienda/src/main/java/org/grupo1/tienda/model/auxiliary/LineaCatalogo.java
+++ tienda/src/main/java/org/grupo1/tienda/model/auxiliary/LineaCatalogo.java
@@ -4,9 +4,6 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.grupo1.tienda.model.entity.Producto;
-
-import java.math.BigDecimal;
 
 @Entity
 @Table(uniqueConstraints = { @UniqueConstraint(name = "UQ_linea_catalogo_producto", columnNames = { "producto_id" }) })
@@ -15,8 +12,5 @@ public class LineaCatalogo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @OneToOne
-    @JoinColumn(foreignKey = @ForeignKey(name = "FK_linea_catalogo_producto_id"))
-    private Producto producto;
-    private BigDecimal precioUnitario;
+    private Integer producto;
 }
