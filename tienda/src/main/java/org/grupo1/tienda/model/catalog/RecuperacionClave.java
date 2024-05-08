6835bd46c6d154ba398307411b895308a1823c90
Azufer <ana.zurita.fdez@gmail.com>
2024-05-08 13:15:06 +0200

    En proceso de validar que la pregunta de la clave sea una de la lista de las preguntas.

--- tienda/src/main/java/org/grupo1/tienda/model/catalog/RecuperacionClave.java
+++ tienda/src/main/java/org/grupo1/tienda/model/catalog/RecuperacionClave.java
@@ -6,15 +6,18 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.grupo1.tienda.model.validation.PreguntaClave;
 
 @Entity
 @AllArgsConstructor @NoArgsConstructor @Data
+
 public class RecuperacionClave {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @ManyToOne
     @JoinColumn(foreignKey = @ForeignKey(name = "FK_recuperacion_clave_pregunta_id"))
+    @PreguntaClave
     @NotNull
     private PreguntaRecuperacion pregunta;
     @NotBlank
