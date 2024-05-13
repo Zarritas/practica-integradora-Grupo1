fe943af5c0f52fa88a6fe8c0d748fd7316c8ad47
Jesus Lorenzo Limon <jlorenzolimon@gmail.com>
2024-05-13 14:06:47 +0200

    comit de agregacion de datos de mongo

--- tienda/src/main/java/org/grupo1/tienda/controller/RestControllerMongo.java
+++ tienda/src/main/java/org/grupo1/tienda/controller/RestControllerMongo.java
@@ -17,8 +17,6 @@ import org.springframework.web.multipart.MultipartFile;
 import java.time.LocalDate;
 import java.util.*;
 
-import static org.grupo1.tienda.service.ServicioMongo.*;
-
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/producto")
@@ -61,12 +59,15 @@ public class RestControllerMongo {
         }
         return resultado;
     }
-
+    public String obtenerTipoDato(Object valor) {
+        return valor == null ? "null" : valor.getClass().getSimpleName();
+    }
 
     @PostMapping("/crear")
     public ResponseEntity<Map<String,Object>> crearProducto(@RequestParam Map<String,String> todosLosParametros, @RequestParam MultipartFile imagen_perfil, @RequestParam List<MultipartFile> imagenes) {
         Map<String, Object> response = new HashMap<>();
         Map<String,String> camposConErrores = new HashMap<>();
+        List <Atributo> atributos = new ArrayList<>();
 
         if (todosLosParametros.isEmpty()) {
             camposConErrores.put("general", "No se han proporcionado datos para guardar.");
@@ -76,7 +77,6 @@ public class RestControllerMongo {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
         }
 
-        List <Atributo> atributos = new ArrayList<>();
         Document producto = new Document();
         Document ultimoId = conexionMongo.find()
                 .projection(Projections.include("_id"))
@@ -85,6 +85,8 @@ public class RestControllerMongo {
                 .first();
 
         producto.append("_id", ultimoId != null ? ultimoId.getInteger("_id") + 1 : 1);
+        producto.append("fecha_creacion", LocalDate.now());
+        producto.append("fecha_ultima_modificacion", LocalDate.now());
 
         for(String atributo: todosLosParametros.keySet())
             if (atributo.startsWith("_"))
@@ -212,10 +214,12 @@ public class RestControllerMongo {
             response.put("camposConErrores", camposConErrores);
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
         }
+
         // Si no hay errores, insertar el producto en la base de datos
         conexionMongo.insertOne(producto);
         response.put("correcto", true);
         response.put("message", "Producto creado correctamente.");
+
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(response);
     }
 
