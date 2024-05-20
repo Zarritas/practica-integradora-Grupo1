c5cbf3ce6ba029ef16c6a1d9a37b0c897c0a9740
Alopezherraiz <alopezherraiz@educa.madrid.org>
2024-05-20 07:41:31 +0200

    Redirección en registro si usuario no iniciado

--- tienda/src/main/java/org/grupo1/tienda/controller/ControllerRegistroCliente.java
+++ tienda/src/main/java/org/grupo1/tienda/controller/ControllerRegistroCliente.java
@@ -25,7 +25,7 @@ import java.util.List;
 @Controller
 @RequestMapping("alta-cliente")
 public class ControllerRegistroCliente {
-    private String prefijoDirectory = "register/";
+    private final String prefijoDirectory = "register/";
     @Autowired
     ServicioPais servicioPais;
     @Autowired
@@ -69,15 +69,19 @@ public class ControllerRegistroCliente {
 @GetMapping("/datos-personales")
     public ModelAndView datosPersonales(ModelAndView modelAndView, HttpSession sesionRegistro,
                                         @ModelAttribute("cliente") Cliente cliente) {
+    if(servicioSesion.getUsuarioLoggeado() == null){
+        modelAndView.setViewName("redirect:/usuario/authusuario");
+    }else {
+        modelAndView.addObject("usuarioLogged", servicioSesion.getUsuarioLoggeado().getEmail());
+        Cliente clienteRegistro = (Cliente) sesionRegistro.getAttribute("cliente");
 
-    Cliente clienteRegistro = (Cliente) sesionRegistro.getAttribute("cliente");
+        if (clienteRegistro != null) {
+            modelAndView.addObject("cliente", clienteRegistro);
+        }
 
-    if(clienteRegistro != null){
-        modelAndView.addObject("cliente", clienteRegistro);
+        modelAndView.addObject("readOnly", false);
+        modelAndView.setViewName(prefijoDirectory + "registro-datos-personales");
     }
-
-    modelAndView.addObject("readOnly", false);
-    modelAndView.setViewName(prefijoDirectory + "registro-datos-personales");
     return modelAndView;
     }
 
@@ -86,16 +90,21 @@ public class ControllerRegistroCliente {
                                             HttpSession sesionRegistro,
                                             @Validated(DatosPersonales.class) @ModelAttribute("cliente") Cliente cliente,
                                             BindingResult resultadoVinculacion) {
-        modelAndView.addObject("readOnly", false);
-        if(resultadoVinculacion.hasErrors()){
-
-            modelAndView.setViewName(prefijoDirectory +"registro-datos-personales");
+        if(servicioSesion.getUsuarioLoggeado() == null){
+            modelAndView.setViewName("redirect:/usuario/authusuario");
         }else{
+            modelAndView.addObject("usuarioLogged", servicioSesion.getUsuarioLoggeado().getEmail());
+            modelAndView.addObject("readOnly", false);
+            if(resultadoVinculacion.hasErrors()){
 
-            sesionRegistro.setAttribute("cliente", cliente);
+                modelAndView.setViewName(prefijoDirectory +"registro-datos-personales");
+            }else{
+
+                sesionRegistro.setAttribute("cliente", cliente);
 
 
-            modelAndView.setViewName("redirect:datos-contacto");
+                modelAndView.setViewName("redirect:datos-contacto");
+            }
         }
         return modelAndView;
     }
@@ -104,17 +113,21 @@ public class ControllerRegistroCliente {
                                        @ModelAttribute("cliente") Cliente cliente,
                                       @ModelAttribute("direccion") Direccion direccion
                                      ) {
-        Cliente clienteRegistro = (Cliente) sesionRegistro.getAttribute("cliente");
+        if(servicioSesion.getUsuarioLoggeado() == null){
+            modelAndView.setViewName("redirect:/usuario/authusuario");
+        }else {
+            modelAndView.addObject("usuarioLogged", servicioSesion.getUsuarioLoggeado().getEmail());
+            Cliente clienteRegistro = (Cliente) sesionRegistro.getAttribute("cliente");
 
-        if(clienteRegistro != null){
-            modelAndView.addObject("cliente", clienteRegistro);
-            if (clienteRegistro.getDireccion() != null){
-                modelAndView.addObject("direccion", clienteRegistro.getDireccion());
+            if (clienteRegistro != null) {
+                modelAndView.addObject("cliente", clienteRegistro);
+                if (clienteRegistro.getDireccion() != null) {
+                    modelAndView.addObject("direccion", clienteRegistro.getDireccion());
+                }
             }
+            modelAndView.setViewName(prefijoDirectory + "registro-datos-contacto");
+            modelAndView.addObject("readOnly", false);
         }
-        modelAndView.setViewName( prefijoDirectory +"registro-datos-contacto");
-        modelAndView.addObject("readOnly", false);
-
         return modelAndView;
     }
     @PostMapping("/datos-contacto")
@@ -123,25 +136,29 @@ public class ControllerRegistroCliente {
                                           BindingResult resultadoVinculacionCliente,
                                           @Validated(DatosContacto.class) @ModelAttribute("direccion") Direccion direccion,
                                           BindingResult resultadoVinculacionDireccion) {
+        if(servicioSesion.getUsuarioLoggeado() == null){
+            modelAndView.setViewName("redirect:/usuario/authusuario");
+        }else {
+            modelAndView.addObject("usuarioLogged", servicioSesion.getUsuarioLoggeado().getEmail());
+            modelAndView.addObject("readOnly", false);
+            if (resultadoVinculacionCliente.hasErrors() || resultadoVinculacionDireccion.hasErrors()) {
 
-        modelAndView.addObject("readOnly", false);
-        if (resultadoVinculacionCliente.hasErrors() || resultadoVinculacionDireccion.hasErrors()) {
+                modelAndView.setViewName(prefijoDirectory + "registro-datos-contacto");
+                return modelAndView;
+            }
 
-            modelAndView.setViewName(prefijoDirectory +"registro-datos-contacto");
-            return modelAndView;
+            Cliente clienteRegistro = (Cliente) sesionRegistro.getAttribute("cliente");
+            if (clienteRegistro != null) {
+                clienteRegistro.setTelefonoMovil(cliente.getTelefonoMovil());
+                clienteRegistro.setDireccion(direccion);
+                sesionRegistro.setAttribute("cliente", clienteRegistro);
+            } else {
+                cliente.setDireccion(direccion);
+                sesionRegistro.setAttribute("cliente", cliente);
+            }
+
+            modelAndView.setViewName("redirect:datos-cliente");
         }
-
-        Cliente clienteRegistro = (Cliente) sesionRegistro.getAttribute("cliente");
-        if (clienteRegistro != null) {
-            clienteRegistro.setTelefonoMovil(cliente.getTelefonoMovil());
-            clienteRegistro.setDireccion(direccion);
-            sesionRegistro.setAttribute("cliente", clienteRegistro);
-        } else {
-            cliente.setDireccion(direccion);
-            sesionRegistro.setAttribute("cliente", cliente);
-        }
-
-        modelAndView.setViewName("redirect:datos-cliente");
         return modelAndView;
     }
     @GetMapping("/datos-cliente")
@@ -149,23 +166,27 @@ public class ControllerRegistroCliente {
                                      @ModelAttribute("cliente") Cliente cliente,
                                      @ModelAttribute("direccionentrega") Direccion direccion,
                                      @ModelAttribute("tarjeta") TarjetaCredito tarjeta) {
+        if(servicioSesion.getUsuarioLoggeado() == null){
+            modelAndView.setViewName("redirect:/usuario/authusuario");
+        }else {
+            modelAndView.addObject("usuarioLogged", servicioSesion.getUsuarioLoggeado().getEmail());
+            Cliente clienteRegistro = (Cliente) sesionRegistro.getAttribute("cliente");
 
-        Cliente clienteRegistro = (Cliente) sesionRegistro.getAttribute("cliente");
+            if (clienteRegistro != null) {
+                modelAndView.addObject("cliente", clienteRegistro);
+                if (!clienteRegistro.getDireccionesEntrega().isEmpty()) {
+                    modelAndView.addObject("direccionentrega", clienteRegistro.getDireccionesEntrega().iterator().next());
 
-        if (clienteRegistro != null) {
-            modelAndView.addObject("cliente", clienteRegistro);
-            if (!clienteRegistro.getDireccionesEntrega().isEmpty()) {
-                modelAndView.addObject("direccionentrega", clienteRegistro.getDireccionesEntrega().iterator().next());
-
+                }
+                if (!clienteRegistro.getTarjetasCredito().isEmpty()) {
+                    modelAndView.addObject("tarjeta", clienteRegistro.getTarjetasCredito().iterator().next());
+                }
             }
-            if (!clienteRegistro.getTarjetasCredito().isEmpty()){
-            modelAndView.addObject("tarjeta", clienteRegistro.getTarjetasCredito().iterator().next());
-            }
+
+            modelAndView.setViewName(prefijoDirectory + "registro-datos-cliente");
+            modelAndView.addObject("readOnly", false);
+            modelAndView.addObject("entrega", true);
         }
-
-        modelAndView.setViewName( prefijoDirectory +"registro-datos-cliente");
-        modelAndView.addObject("readOnly", false);
-        modelAndView.addObject("entrega", true);
         return modelAndView;
     }
     @PostMapping("/datos-cliente")
@@ -176,28 +197,30 @@ public class ControllerRegistroCliente {
                                          BindingResult resultadoVinculacionDireccion,
                                          @Validated(DatosCliente.class) @ModelAttribute("tarjeta") TarjetaCredito tarjeta,
                                          BindingResult resultadoVinculacionTarjeta) {
-
-        modelAndView.addObject("readOnly", false);
-        if (resultadoVinculacionCliente.hasErrors() || resultadoVinculacionDireccion.hasErrors() || resultadoVinculacionTarjeta.hasErrors()) {
-            modelAndView.setViewName(prefijoDirectory +"registro-datos-cliente");
-            return modelAndView;
-        } else {
-            modelAndView.setViewName("redirect:confirmar-registro");
-        }
-        Cliente clienteRegistro = (Cliente) sesionRegistro.getAttribute("cliente");
-        if (clienteRegistro != null) {
-            clienteRegistro.getDireccionesEntrega().add(direccion);
-            clienteRegistro.getTarjetasCredito().add(tarjeta);
-            clienteRegistro.setComentarios(cliente.getComentarios());
-            modelAndView.addObject("cliente", clienteRegistro);
+        if(servicioSesion.getUsuarioLoggeado() == null){
+            modelAndView.setViewName("redirect:/usuario/authusuario");
         }else {
-            cliente.getDireccionesEntrega().add(direccion);
-            cliente.getTarjetasCredito().add(tarjeta);
-            cliente.setComentarios(cliente.getComentarios());
-            sesionRegistro.setAttribute("cliente", cliente);
+            modelAndView.addObject("usuarioLogged", servicioSesion.getUsuarioLoggeado().getEmail());
+            modelAndView.addObject("readOnly", false);
+            if (resultadoVinculacionCliente.hasErrors() || resultadoVinculacionDireccion.hasErrors() || resultadoVinculacionTarjeta.hasErrors()) {
+                modelAndView.setViewName(prefijoDirectory + "registro-datos-cliente");
+                return modelAndView;
+            } else {
+                modelAndView.setViewName("redirect:confirmar-registro");
+            }
+            Cliente clienteRegistro = (Cliente) sesionRegistro.getAttribute("cliente");
+            if (clienteRegistro != null) {
+                clienteRegistro.getDireccionesEntrega().add(direccion);
+                clienteRegistro.getTarjetasCredito().add(tarjeta);
+                clienteRegistro.setComentarios(cliente.getComentarios());
+                modelAndView.addObject("cliente", clienteRegistro);
+            } else {
+                cliente.getDireccionesEntrega().add(direccion);
+                cliente.getTarjetasCredito().add(tarjeta);
+                cliente.setComentarios(cliente.getComentarios());
+                sesionRegistro.setAttribute("cliente", cliente);
+            }
         }
-
-
         return modelAndView;
     }
 
@@ -207,52 +230,65 @@ public class ControllerRegistroCliente {
                                         @ModelAttribute("direccion") Direccion direccion,
                                         @ModelAttribute("direccionentrega") Direccion direccionentrega,
                                         @ModelAttribute("tarjeta") TarjetaCredito tarjeta) {
-        Cliente clienteRegistro = (Cliente) sesionRegistro.getAttribute("cliente");
-        if (clienteRegistro != null) {
+        if(servicioSesion.getUsuarioLoggeado() == null){
+            modelAndView.setViewName("redirect:/usuario/authusuario");
+        }else {
+            modelAndView.addObject("usuarioLogged", servicioSesion.getUsuarioLoggeado().getEmail());
+            Cliente clienteRegistro = (Cliente) sesionRegistro.getAttribute("cliente");
+            if (clienteRegistro != null) {
 
                 modelAndView.addObject("cliente", clienteRegistro);
-                if(clienteRegistro.getDireccion() != null){
+                if (clienteRegistro.getDireccion() != null) {
                     modelAndView.addObject("direccion", clienteRegistro.getDireccion());
                 }
-                if(!clienteRegistro.getDireccionesEntrega().isEmpty()){
+                if (!clienteRegistro.getDireccionesEntrega().isEmpty()) {
                     Direccion direccion1 = clienteRegistro.getDireccionesEntrega().iterator().next();
                     modelAndView.addObject("direccionentrega", direccion1);
                 }
-                if(!clienteRegistro.getTarjetasCredito().isEmpty()){
+                if (!clienteRegistro.getTarjetasCredito().isEmpty()) {
                     TarjetaCredito tarjeta1 = clienteRegistro.getTarjetasCredito().iterator().next();
                     modelAndView.addObject("tarjeta", tarjeta1);
                 }
+            }
+            modelAndView.setViewName(prefijoDirectory + "registro-datos-resumen");
+            modelAndView.addObject("readOnly", true);
         }
-        modelAndView.setViewName(prefijoDirectory +"registro-datos-resumen");
-        modelAndView.addObject("readOnly", true);
         return modelAndView;
     }
     @PostMapping("/confirmar-registro")
-
-
     public ModelAndView confirmarRegistroPost(ModelAndView modelAndView, HttpSession sesionRegistro,
+                                              @ModelAttribute("direccion") Direccion direccion,
+                                              @ModelAttribute("direccionentrega") Direccion direccionentrega,
+                                              @ModelAttribute("tarjeta") TarjetaCredito tarjeta,
                                               @Validated(DatosResumen.class)@ModelAttribute("cliente") Cliente cliente,
                                               BindingResult resultadoVinculacion) {
-        modelAndView.addObject("readOnly", true);
-        Cliente clienteRegistro = (Cliente) sesionRegistro.getAttribute("cliente");
-        System.err.println(clienteRegistro);
-        if(resultadoVinculacion.hasErrors()
-                || clienteRegistro == null) {
-            modelAndView.setViewName(prefijoDirectory +"registro-datos-resumen");
-            return modelAndView;
+
+        if(servicioSesion.getUsuarioLoggeado() == null){
+            modelAndView.setViewName("redirect:/usuario/authusuario");
         }else{
-            UsuarioEmpleadoCliente usuario = servicioSesion.getUsuarioLoggeado();
+            modelAndView.addObject("usuarioLogged", servicioSesion.getUsuarioLoggeado().getEmail());
+
+            modelAndView.addObject("readOnly", true);
+            Cliente clienteRegistro = (Cliente) sesionRegistro.getAttribute("cliente");
             System.err.println(clienteRegistro);
-            clienteRegistro.setUsuario(usuario);
-            servicioDireccion.guardarDireccion(clienteRegistro.getDireccion());
-            servicioDireccion.guardarDirecciones(clienteRegistro.getDireccionesEntrega());
-            servicioTarjetaCredito.guardarTarjetas(clienteRegistro.getTarjetasCredito());
-            servicioCliente.guardarCliente(clienteRegistro);
-            modelAndView.setViewName("app/area_personal");
-            sesionRegistro.removeAttribute("cliente");
+            if(resultadoVinculacion.hasErrors()
+                    || clienteRegistro == null) {
+                modelAndView.setViewName(prefijoDirectory +"registro-datos-resumen");
+                return modelAndView;
+            }else{
+                UsuarioEmpleadoCliente usuario = servicioSesion.getUsuarioLoggeado();
+                System.err.println(clienteRegistro);
+                clienteRegistro.setUsuario(usuario);
+                servicioDireccion.guardarDireccion(clienteRegistro.getDireccion());
+                servicioDireccion.guardarDirecciones(clienteRegistro.getDireccionesEntrega());
+                servicioTarjetaCredito.guardarTarjetas(clienteRegistro.getTarjetasCredito());
+                servicioCliente.guardarCliente(clienteRegistro);
+                modelAndView.setViewName("app/area_personal");
+                sesionRegistro.removeAttribute("cliente");
+            }
         }
         return modelAndView;
-                                             }
+    }
     @GetMapping("masacre")
     public ModelAndView borrarDatos(ModelAndView modelAndView, HttpSession sesionRegistro) {
         sesionRegistro.removeAttribute("cliente");
